7f0eed48bb31f520c5525af9d2ae163a373a14eb
MacNikSipeykin <niksipeykin@mail.ru>
2023-05-26 16:54:24 +0300

    complete homework lesson 4

--- src/ru/geekBrains/java/sypeykinNV/webinar/lesson_4_NAHW_NET_CHAT/ClientGUI.java
+++ src/ru/geekBrains/java/sypeykinNV/webinar/lesson_4_NAHW_NET_CHAT/ClientGUI.java
@@ -1,17 +1,22 @@
 package ru.geekBrains.java.sypeykinNV.webinar.lesson_4_NAHW_NET_CHAT;
 
+import javax.imageio.IIOException;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class ClientGUI extends JFrame implements ActionListener, Thread.UncaughtExceptionHandler {
 /*
 ДЗ к уроку 4
-* todo доработатть программу так, чтобы она могла отправлять сообщения в лог по нажатию кнопки или по нажатию клавиши
+*  доработатть программу так, чтобы она могла отправлять сообщения в лог по нажатию кнопки  или по нажатию клавиши
    enter.
-* todo Создать лог в файле (показать комментарием, где и как Вы планируете писать сообщение в файловый журнал).
-* todo Прочитать методичку к следующему уроку
+*  Создать лог в файле (показать комментарием, где и как Вы планируете писать сообщение в файловый журнал).
+*  Прочитать методичку к следующему уроку
 * */
 
     private static final int WIDTH = 400;
@@ -32,6 +37,7 @@ public class ClientGUI extends JFrame implements ActionListener, Thread.Uncaught
     private final JButton buttonSend = new JButton("Send");
 
     private final JList<String> userList = new JList<>();
+    private boolean shownIoErrors = false;
 
     public static void main(String[] args) {
 
@@ -69,6 +75,20 @@ public class ClientGUI extends JFrame implements ActionListener, Thread.Uncaught
         panelBottom.add(textFieldMessage, BorderLayout.CENTER);
         panelBottom.add(buttonSend, BorderLayout.EAST);
 
+        buttonSend.addActionListener(this);
+        //Ivan realization
+        textFieldMessage.addActionListener(this);
+
+        //my realization
+//        textFieldMessage.addKeyListener(new KeyAdapter() {
+//            @Override
+//            public void keyPressed(KeyEvent e) {
+//                if (e.getKeyCode() == KeyEvent.VK_ENTER){
+//                    sender();
+//                }
+//            }
+//        });
+
         add(scrollLog, BorderLayout.CENTER);
         add(scrollUser, BorderLayout.EAST);
         add(panelTop, BorderLayout.NORTH);
@@ -81,20 +101,80 @@ public class ClientGUI extends JFrame implements ActionListener, Thread.Uncaught
         Object src = e.getSource();
         if(src == checkBoxAlwaysOnTop){
             setAlwaysOnTop(checkBoxAlwaysOnTop.isSelected());
-        }else{
+        } else if (src == buttonSend || src == textFieldMessage) {
+            sendMessage();
+        } else{
             throw new RuntimeException("Unknown Source: " + src);
         }
     }
 
+    /** Message send methods **/
+
+    private void sendMessage(){
+        String msg = textFieldMessage.getText();
+        String username = textFieldLogin.getText();
+        if("".equals(msg)) return;
+        textFieldMessage.setText(null);
+        textFieldMessage.grabFocus();
+        putLog(String.format("%s: %s", username, msg));
+        writeMsgToLogFile(msg, username);
+    }
+
+    private void writeMsgToLogFile(String msg, String username) {
+        try (FileWriter out = new FileWriter("log.txt", true)){
+            out.write(username + ": " + msg + "\n");
+            out.flush();
+        }catch (IOException e){
+            if (!shownIoErrors){
+                shownIoErrors = true;
+                showException(Thread.currentThread(), e);
+            }
+        }
+    }
+
+    private void putLog(String msg) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                log.append(msg + "\n");
+                log.setCaretPosition(log.getDocument().getLength());
+            }
+        });
+    }
+
+    //my realization
+    private void sender() {
+
+        String msg =  textFieldMessage.getText();
+        textFieldMessage.setText("");
+
+        log.append(msg + "\n");
+    }
+
+    /** Message send methods **/
+
+    /** exception methods **/
+
     @Override
     public void uncaughtException(Thread t, Throwable e) {
         e.printStackTrace();
-        String msg;
-        StackTraceElement[] stackTraceElements = e.getStackTrace();
-        msg = "Exception" + t.getName() + " " +
-                e.getClass().getCanonicalName() + ": " +
-                e.getMessage() + "\n\t at " + stackTraceElements[0];
-        JOptionPane.showMessageDialog(this, msg, "Exception", JOptionPane.ERROR_MESSAGE);
+        showException(t, e);
         System.exit(1);
     }
+
+    private void showException(Thread t, Throwable e) {
+        String msg;
+        StackTraceElement[] stackTraceElements = e.getStackTrace();
+        if (stackTraceElements.length == 0)
+            msg = "Empty Stacktrace";
+        else {
+            msg = String.format("Exception in \"%s\" %s\n\tat $s", t.getName(), e.getClass().getCanonicalName(),
+                    e.getMessage(),stackTraceElements[0]);
+            JOptionPane.showMessageDialog(this, msg, "Exception", JOptionPane.ERROR_MESSAGE);
+        }
+
+        JOptionPane.showMessageDialog(null, msg, "Exception", JOptionPane.ERROR_MESSAGE);
+    }
+
+    /** exception methods **/
 }
